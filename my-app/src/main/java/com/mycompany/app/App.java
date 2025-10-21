363b1a07d2f7f3c7844df549ef93c447a8312f64
ph-ud <215398534+ph-ud@users.noreply.github.com>
2025-10-21 12:36:57 +0200

    bad sample change

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Sample Change");
+
+            System.out.println("Bad Sample Change")  ;
     }
 }
