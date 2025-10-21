ca325003f8d5c3b4ca6fdaf20cc6bbbdb5c6412e
ph-ud <215398534+ph-ud@users.noreply.github.com>
2025-10-21 12:49:24 +0200

    bad sample change 2

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,10 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Sample Change");
 
-            System.out.println("Bad Sample Change")  ;
+        // The following line is a deliberate mistake
+        System.out.println("Bad Sample Change")  ;  // Missing semicolon
+
+        System.out.println("This line will never execute" ); // This comment is misleading
+        System.out.println(null); // Printing null could be considered bad practice
     }
 }
