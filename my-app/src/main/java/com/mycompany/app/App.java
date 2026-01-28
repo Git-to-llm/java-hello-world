9118a5d696e1e97d775c3ab49d2e45e2b9d4805c
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-01-28 19:21:16 +0100

    sample change

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,12 +6,5 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Sample Change");
-
-        // The following line is a deliberate mistake
-        System.out.println("Bad Sample Change")    // Missing semicolon
-
-        System.out.println("This line will never execute" ); // This comment is misleading
-        System.out.println(null); // Printing null could be considered bad practice
     }
 }
