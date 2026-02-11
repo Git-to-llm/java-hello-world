c53c34bce2238613c7e012160674f9d445359994
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-02-12 00:40:15 +0100

    sample change

--- my-app/src/test/java/com/mycompany/app/HiveTablesTest.java
+++ my-app/src/test/java/com/mycompany/app/HiveTablesTest.java
@@ -11,9 +11,9 @@ public class HiveTablesTest extends HiveTableBaseTest {
 
    @Test(expected = CommitFailedException.class)
    public void testFailure() throws TException {
-      org.apache.iceberg.Table icebergTable = new HiveTables(hiveConf).load(DB_NAME, TABLE_NAME);
-      final Table table = metastoreClient.getTable(DB_NAME, TABLE_NAME);
-      final String dummyLocation = "dummylocation";
+      Table icebergTable = tables.load(DB_NAME, TABLE_NAME);
+      org.apache.hadoop.hive.metastore.api.Table table = metastoreClient.getTable(DB_NAME, TABLE_NAME);
+      String dummyLocation = "dummylocation";
       table.getParameters().put(METADATA_LOCATION_PROP, dummyLocation);
       metastoreClient.alter_table(DB_NAME, TABLE_NAME, table);
       icebergTable.updateSchema()
