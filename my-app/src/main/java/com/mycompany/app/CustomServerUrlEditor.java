65ee55d287790042ec542f8e841c836bde4fe0ef
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-02-12 02:11:23 +0100

    Test Change

--- my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
+++ my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
@@ -2,7 +2,7 @@ func runExecutions(
  		if nonce, ok = nonces[executor]; !ok {
  			state, err := accountutil.AccountState(sf, executor)
  			if err != nil {
-				return nil, err, nil
+				return nil, err
  			}
  			nonce = state.Nonce
  		}
