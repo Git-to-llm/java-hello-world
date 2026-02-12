df9a0004893f9a1aad78e03559e3145ac80c4dfd
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-02-12 03:00:43 +0100

    Test Change

--- my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
+++ my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
@@ -1,8 +1,17 @@
-@@ -115,7 +115,7 @@ func AccountStateAtHeight(sr protocol.StateReader, encodedAddr string, height ui
- 	}
- 	pkHash := hash.BytesToHash160(addr.Bytes())
- 	var account state.Account
-	if _, err := sr.State(&account, protocol.BlockHeightOption(height), protocol.LegacyKeyOption(pkHash)); err != nil {
- 		if errors.Cause(err) == state.ErrStateNotExist {
- 			account = state.EmptyAccount()
- 			return &account, nil
+@@ -1192,15 +1192,16 @@ type BlockServer interface {
+ 	// If this returns a BServerErrorOverQuota, with Throttled=false,
+ 	// the caller can treat it as informational and otherwise ignore
+ 	// the error.
+	Put(ctx context.Context, tlfID tlf.ID, id BlockID, context BlockContext,
+ 
+ 	// AddBlockReference adds a new reference to the given block,
+	// defined by the given context (which should contain a non-zero
+	// BlockRefNonce).  (Contexts with a BlockRefNonce of zero should
+	// be used when putting the block for the first time via Put().)
+	// Returns a BServerErrorBlockNonExistent if id is unknown within
+	// this folder.
+ 	//
+ 	// AddBlockReference should be idempotent, although it should
+ 	// also return an error if, for a given ID and refnonce, any
+
+
