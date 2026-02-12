7d041441b8f056fcb9dc406c09c0c400c04abdf6
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-02-12 02:58:23 +0100

    Test Change

--- my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
+++ my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
@@ -2,7 +2,7 @@
  	}
  	pkHash := hash.BytesToHash160(addr.Bytes())
  	var account state.Account
-	if err := sr.State(pkHash, &account, protocol.BlockHeightOption(height)); err != nil {
+	if _, err := sr.State(&account, protocol.BlockHeightOption(height), protocol.LegacyKeyOption(pkHash)); err != nil {
  		if errors.Cause(err) == state.ErrStateNotExist {
  			account = state.EmptyAccount()
  			return &account, nil
