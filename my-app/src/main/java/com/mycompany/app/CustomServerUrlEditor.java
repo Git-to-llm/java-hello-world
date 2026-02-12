938603de5f591570bab2aaca074b1114da8a22fa
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-02-12 02:56:59 +0100

    Test Change

--- my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
+++ my-app/src/main/java/com/mycompany/app/CustomServerUrlEditor.java
@@ -1,8 +1,8 @@
-func runExecutions(
- 		if nonce, ok = nonces[executor]; !ok {
- 			state, err := accountutil.AccountState(sf, executor)
- 			if err != nil {
-				return nil, err
- 			}
- 			nonce = state.Nonce
- 		}
+@@ -115,7 +115,7 @@ func AccountStateAtHeight(sr protocol.StateReader, encodedAddr string, height ui
+ 	}
+ 	pkHash := hash.BytesToHash160(addr.Bytes())
+ 	var account state.Account
+	if err := sr.State(pkHash, &account, protocol.BlockHeightOption(height)); err != nil {
+ 		if errors.Cause(err) == state.ErrStateNotExist {
+ 			account = state.EmptyAccount()
+ 			return &account, nil
