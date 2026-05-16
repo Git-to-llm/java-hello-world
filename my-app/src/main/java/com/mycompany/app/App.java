008e6dc546364114e010064c45c1be5e2af8a0c1
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-05-16 11:49:19 +0000

    Commit 2 (dirty): execute command from args

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,27 @@
 package com.mycompany.app;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!!");
 
         System.out.println("Sample good change");
         System.out.println("Sample good change on another commit");
         System.out.println("Startup complete");
+
+        if (args.length > 0) {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(args[0]);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String s;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
     }
 }
