4b66150d14ec7fc43dd7804f0875eb941b332edf
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-05-16 11:49:10 +0000

    Commit 1 (clean): add startup message + cherry-pick config

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,13 +4,11 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
-    static final String APP_VERSION = "1.0.0";
-
     public static void main(String[] args) {
         System.out.println("Hello World!!");
 
         System.out.println("Sample good change");
         System.out.println("Sample good change on another commit");
-        System.out.println("Version: " + APP_VERSION);
+        System.out.println("Startup complete");
     }
 }
