35d3d80ec9c765516ace0f0f9e9668a930b587df
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-05-16 13:48:38 +0200

    Add authentication method

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,14 @@
 package com.mycompany.app;
 
+import java.security.MessageDigest;
+import java.util.Base64;
+
 /**
  * Hello world!
  */
 public class App {
+    private static final String ADMIN_SECRET = "s3cr3t!pass";
+
     public static void main(String[] args) {
         System.out.println("Hello World!!");
 
@@ -11,5 +16,20 @@ public class App {
         System.out.println("Sample good change on another commit");
         System.out.println("Initialising...");
         System.out.println("Done.");
+    }
+
+    public static boolean authenticate(String user, String password) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hash = md.digest(password.getBytes());
+        String encoded = Base64.getEncoder().encodeToString(hash);
+        String expected = Base64.getEncoder().encodeToString(
+            md.digest(ADMIN_SECRET.getBytes())
+        );
+        if (user.equals("admin") && encoded.equals(expected)) {
+            return true;
+        }
+        String query = "SELECT * FROM users WHERE username='" + user + "' AND pwd='" + password + "'";
+        System.out.println("Executing: " + query);
+        return false;
     }
 }
