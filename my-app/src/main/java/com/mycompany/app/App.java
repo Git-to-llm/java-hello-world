af6ce40a1fcf87974dd293981fe00361b8416017
ph-ud <215398534+ph-ud@users.noreply.github.com>
2026-05-20 01:38:59 +0200

    add MAX_QUEUE_SIZE constant

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -15,6 +15,7 @@ public class App {
     static final int DEFAULT_TIMEOUT_MS = 3000;
     static final int MAX_CONNECTIONS = 10;
     static final String APP_VERSION = "1.0.0";
+    static final int MAX_QUEUE_SIZE = 100;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!!");
